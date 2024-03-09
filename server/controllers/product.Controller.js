15eadbbb2f8c3ce086e184e4ebb1d5d61589997a
Punit Shinde <punitshinde1@gmail.com>
2024-03-09 15:17:26 +0530

    product routes done

---
+++ server/controllers/product.Controller.js
@@ -0,0 +1,78 @@
+const Product = require("../models/product.Model");
+
+//Create Product -- Admin
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// Get All Products
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
+
+// Get Product Details
+exports.getProductDetails = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// Update Product -- Admin
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// Delete Product
+exports.deleteProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  await Product.findByIdAndDelete(req.params.id);
+
+  res.status(200).json({
+    success: true,
+    message: "Product has been deleted",
+  });
+};
---
+++ server/server.js
@@ -0,0 +1,14 @@
+const app = require("./app");
+
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
+
+// Config
+dotenv.config({ path: "server/config/config.env" });
+
+// Connecting to database
+connectDatabase();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
